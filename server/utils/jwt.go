127f7a148400369d045d4e51988f788f112dcb86
TeddyMuli <tmuli974@gmail.com>
2025-01-05 20:52:48 +0300

    Create utility functions for jwt

---
+++ server/utils/jwt.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	//"github.com/google/uuid"
+)
+
+var secretKey = []byte(os.Getenv("JWT_SECRET"))
+
+// GenerateToken generates a JWT token with the user ID as part of the claims
+func GenerateToken(userID string, email string, firstName string, lastName string) (string, error) {
+    claims := jwt.MapClaims{}
+    claims["user_id"] = userID
+    claims["email"] = email
+    claims["first_name"] = firstName
+    claims["last_name"] = lastName
+    claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    signedToken, err := token.SignedString(secretKey)
+    if err != nil {
+        return "", err
+    }
+    return signedToken, nil
+}
+
+// VerifyToken verifies a token JWT validate 
+func VerifyToken(tokenString string) (jwt.MapClaims, error) {
+    // Parse the token
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+        // Check the signing method
+        if token.Method != jwt.SigningMethodHS256 {
+            return nil, fmt.Errorf("invalid signing method")
+        }
+
+        return secretKey, nil
+    })
+
+    // Check for errors
+    if err != nil {
+        return nil, err
+    }
+
+    // Validate the token
+    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+        return claims, nil
+    }
+
+    return nil, fmt.Errorf("invalid token")
+}

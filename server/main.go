dbd28ac89547dfaaab956c8e6fb74c8419174d46
TeddyMuli <tmuli974@gmail.com>
2025-01-06 21:19:01 +0300

    update CORS

--- server/main.go
+++ server/main.go
@@ -38,7 +38,7 @@ func main() {
 	// Initialize Fiber router
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-    AllowOrigins:     "http://localhost:3000, http://localhost:5174",
+    AllowOrigins:     "http://localhost:3000, http://localhost:5174, https://alx-silo.vercel.app",
     AllowHeaders:     "Origin, Content-Type, Accept",
     AllowCredentials: true,
 	}))

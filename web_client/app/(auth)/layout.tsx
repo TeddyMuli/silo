fc1b8be157af41f8d8572a06346a6d1893fd8e5d
TeddyMuli <tmuli974@gmail.com>
2025-01-07 11:17:31 +0300

    set app metadata

--- web_client/app/(auth)/layout.tsx
+++ web_client/app/(auth)/layout.tsx
@@ -9,7 +9,6 @@ import ReactQueryProvider from "@/context/ReactQueryProvider";
 const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-poppins',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 });
 

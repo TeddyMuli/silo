4ad5e4f5dd2b1875851653a8c4913ce321612843
TeddyMuli <tmuli974@gmail.com>
2025-01-07 10:53:36 +0300

    add middleware to protect routes

--- server/middleware/auth_middleware.go
+++ server/middleware/auth_middleware.go
@@ -1,25 +1,25 @@
 package utils
 
 import (
-	"os"
-	"time"
+"os"
+"time"
 
-	"github.com/golang-jwt/jwt/v5"
+"github.com/golang-jwt/jwt/v5"
 )
 
 var jwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
 func GenerateToken(userID int, email string) (string, error) {
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-        "user_id": userID,
-        "email": email,
-        "exp":     time.Now().Add(time.Hour * 72).Unix(),
-    })
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"user_id": userID,
+			"email": email,
+			"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	})
 
-    tokenString, err := token.SignedString(jwtSecret)
-    if err != nil {
-        return "", err
-    }
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+			return "", err
+	}
 
-    return tokenString, nil
+	return tokenString, nil
 }

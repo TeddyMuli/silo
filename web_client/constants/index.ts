350e6e3cddf554fa688069ced101700837f93c2d
TeddyMuli <tmuli974@gmail.com>
2025-01-07 11:31:37 +0300

    add react query hook

--- web_client/constants/index.ts
+++ web_client/constants/index.ts
@@ -6,7 +6,6 @@ import JSZip from 'jszip';
 import { fetchFolderHierarchyRecursively } from "@/queries";
 import axios from "axios";
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const useOrganizationId = () => {
   const params = useParams();

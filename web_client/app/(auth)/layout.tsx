7b9268b951498b3dd8eb9b7082e491133dc89a1a
TeddyMuli <tmuli974@gmail.com>
2025-01-07 11:17:56 +0300

    change font to poppins

--- web_client/app/(auth)/layout.tsx
+++ web_client/app/(auth)/layout.tsx
@@ -9,6 +9,7 @@ import ReactQueryProvider from "@/context/ReactQueryProvider";
 const poppins = Poppins({
   subsets: ['latin'],
   display: 'swap',
+  variable: '--font-poppins',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 });
 
